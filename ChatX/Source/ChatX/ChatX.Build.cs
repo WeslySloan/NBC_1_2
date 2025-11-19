cb606a8680299eaf8102a782d6bf711f2a97f178
WeslySloan <88778903+WeslySloan@users.noreply.github.com>
2025-11-19 20:50:15 +0900

    (2025.11.19) BP, UI

--- ChatX/Source/ChatX/ChatX.Build.cs
+++ ChatX/Source/ChatX/ChatX.Build.cs
@@ -1,23 +1,24 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
+// ChatX.Build.cs
 
 using UnrealBuildTool;
 
 public class ChatX : ModuleRules
 {
-	public ChatX(ReadOnlyTargetRules Target) : base(Target)
-	{
-		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+    public ChatX(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
+        PublicDependencyModuleNames.AddRange(new string[]
+        {
+			// Initial Dependencies
+			"Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput",
 
-		// Uncomment if you are using Slate UI
-		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
-		// Uncomment if you are using online features
-		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
+            // UI
+			"UMG", "Slate", "SlateCore",
+        });
 
-		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
-	}
+        PrivateDependencyModuleNames.AddRange(new string[] { });
+
+        PublicIncludePaths.AddRange(new string[] { "ChatX" });
+    }
 }

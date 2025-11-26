4a51467b0e90aa2f1ba6d426ce1d126a2264e7a6
WeslySloan <88778903+WeslySloan@users.noreply.github.com>
2025-11-26 20:51:20 +0900

    (2025.11.26) CXPawn

--- ChatX/Source/ChatX/Player/CXPlayerController.cpp
+++ ChatX/Source/ChatX/Player/CXPlayerController.cpp
@@ -43,7 +43,7 @@ void ACXPlayerController::PrintChatMessageString(const FString& InChatMessageStr
 
 	//FString NetModeString = ChatXFunctionLibrary::GetNetModeString(this);
 	//FString CombinedMessageString = FString::Printf(TEXT("%s: %s"), *NetModeString, *InChatMessageString);
-	ChatXFunctionLibrary::MyPrintString(this, CombinedMessageString, 10.f);
+	//ChatXFunctionLibrary::MyPrintString(this, CombinedMessageString, 10.f);
 	// 문제 상황이 생기면, 위와 같은 로깅 함수로 다양한 변수의 값들과 함수이름을 확인해서 
 	// 문제의 원인을 적극적으로 찾아보세요!
 
